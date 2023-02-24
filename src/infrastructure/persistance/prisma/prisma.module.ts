253aab068a267073dd47a4a0615d3f42e1a48800
srdjan1210 <businessrdjan@gmail.com>
2023-02-24 22:58:03 +0100

    Initiial setup

---
+++ src/infrastructure/persistance/prisma/prisma.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { PrismaProvider } from "./prisma.provider";
+@Module({
+  providers: [PrismaProvider]
+})
+export class PrismaModule {}
---
+++ src/infrastructure/persistance/prisma/prisma.provider.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common/decorators/core/injectable.decorator";
+import { OnModuleInit } from "@nestjs/common/interfaces/hooks/on-init.interface";
+import { INestApplication } from "@nestjs/common/interfaces/nest-application.interface";
+import { PrismaClient } from "@prisma/client";
+@Injectable()
+export class PrismaProvider extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
